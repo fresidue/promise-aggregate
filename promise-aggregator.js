53f008498e129ece600c96d11b50365655713ddf
fresidue <fresidue@gmail.com>
2018-06-17 18:27:04 +0200

    tweaks

--- promise-aggregator.js
+++ promise-aggregator.js
@@ -11,40 +11,20 @@ const modes = {
 
 const defaultOptions = {
   mode: modes.NULL,
-  replaceArgs: (args, prev) => args, // in case we want to accumulate, allow customizer
   minInterval: 300,
   maxWait: 300,
   aggInterval: 0,
+  replaceArgs: (args, prev) => args // in case we want to accumulate, allow customizer
 };
 
-// wrap the aggregate functionality in options handling, so we can separate
-// out the messy validation etc.
-const applyOptions = (inputFn, opts) => {
-  // the fleshed out options
-  const options = Object.assign({}, defaultOptions, opts);
-  assert(typeof options.replaceArgs === 'function', 'replaceArgs is not a function');
-  assert(options.mode === modes.NULL || options.mode === modes.ERROR || options.mode === modes.REPEAT, 'invalid mode');
-  assert(Number.isFinite(options.aggInterval) && options.aggInterval >= 0, 'aggInterval must be >= 0');
-  assert(Number.isFinite(options.minInterval) && options.minInterval >= 0, 'minInterval must be >= 0');
-  assert(Number.isFinite(options.maxWait) || options.maxWait === Infinity, 'maxWait must be a finite number');
-  if (options.maxWait < 0) {
-    options.maxWait = Infinity;
-  }
-  assert(options.maxWait >= options.minInterval, 'maxWait must be >= minInterval');
-
-  const aggregator = createAggregator(inputFn, options);
-  aggregator.options = options;
-  return aggregator;
-};
-
+// Creates aggregated version of the input function
 const createAggregator = (inputFn, options) => {
   // and the mutable state of this object
   const state = {
     args: null,
     scheduled: null,
-    // triggeredAt: 0, // i.e. 1970
     firstTriggeredAt: null,
-    runAt: 0, // i.e. new Date(0).valueOf()
+    lastRunAt: 0, // i.e. new Date(0).valueOf()
   };
 
   // the schedule manages itself
@@ -81,7 +61,7 @@ const createAggregator = (inputFn, options) => {
       // otherwise we definitely run
       // and start a new trailing wait (this can result in !oldState.args)
       state.firstTriggeredAt = null;
-      state.runAt = now;
+      state.lastRunAt = now;
       state.scheduled = schedule(options.minInterval);
       return Promise.resolve(inputFn.apply(null, oldState.args));
     });
@@ -95,7 +75,7 @@ const createAggregator = (inputFn, options) => {
   };
 
   // the main exposed wrapper that handles 'call' events from outside world
-  const wrapper = function (...args) {
+  const wrapper = async function (...args) {
     const oldState = Object.assign({}, state);
     const replacementArgs = options.replaceArgs(args, state.args);
     const now = Date.now();
@@ -104,17 +84,9 @@ const createAggregator = (inputFn, options) => {
     if (!state.scheduled) {
       // console.log('got nothing scheduled');
       state.firstTriggeredAt = now;
-      if (options.leading) {
-        state.runAt = now;
-        state.args = null;
-        state.scheduled = schedule(options.minInterval);
-        return Promise.resolve(inputFn.apply(null, replacementArgs));
-      } else {
-        state.firstTriggeredAt = now;
-        state.args = replacementArgs;
-        state.scheduled = schedule(options.aggInterval);
-        return state.scheduled;
-      }
+      state.args = replacementArgs;
+      state.scheduled = schedule(options.aggInterval);
+      return state.scheduled;
     }
     // otherwise deal with the old scheduled, and create a new
     else {
@@ -122,7 +94,7 @@ const createAggregator = (inputFn, options) => {
       if (!state.firstTriggeredAt) {
         state.firstTriggeredAt = now;
       }
-      const minCallDelay = oldState.runAt + options.minInterval - now;
+      const minCallDelay = oldState.lastRunAt + options.minInterval - now;
       const callDelay = Math.max(minCallDelay, options.aggInterval);
       const maxWaitAllowed = state.firstTriggeredAt + options.maxWait - now;
       const wait = Math.min(callDelay, maxWaitAllowed);
@@ -132,7 +104,7 @@ const createAggregator = (inputFn, options) => {
       if (wait <= 0) {
         state.scheduled = schedule(options.wait);
         state.firstTriggeredAt = null;
-        state.runAt = now;
+        state.lastRunAt = now;
         state.args = null;
         const rv =  Promise.resolve(inputFn.apply(null, replacementArgs));
         oldState.scheduled.cancel(options.mode === modes.REPEAT ? rv : true);
@@ -146,6 +118,26 @@ const createAggregator = (inputFn, options) => {
     }
   };
   return wrapper;
+};
+
+// wrap the aggregate functionality in options handling, so we can separate
+// out the messy validation etc.
+const applyOptions = (inputFn, opts) => {
+  // the fleshed out options
+  const options = Object.assign({}, defaultOptions, opts);
+  assert(typeof options.replaceArgs === 'function', 'replaceArgs is not a function');
+  assert(options.mode === modes.NULL || options.mode === modes.ERROR || options.mode === modes.REPEAT, 'invalid mode');
+  assert(Number.isFinite(options.aggInterval) && options.aggInterval >= 0, 'aggInterval must be >= 0');
+  assert(Number.isFinite(options.minInterval) && options.minInterval >= 0, 'minInterval must be >= 0');
+  assert(Number.isFinite(options.maxWait) || options.maxWait === Number.POSITIVE_INFINITY, 'maxWait must be a finite number');
+  if (options.maxWait < 0) {
+    options.maxWait = Number.POSITIVE_INFINITY;
+  }
+  assert(options.maxWait >= options.minInterval, 'maxWait must be >= minInterval');
+
+  const aggregator = createAggregator(inputFn, options);
+  aggregator.options = options;
+  return aggregator;
 };
 
 module.exports = {
